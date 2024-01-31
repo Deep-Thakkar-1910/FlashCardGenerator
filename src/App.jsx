e63f4fdc3d9c76ecdf8fe6977aaaf8f9aa804966
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-01-31 21:10:03 +0530

    Create Group component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import CreateGroup from "./components/CreateGroup/CreateGroup";
 
 const App = () => {
   return (
     <div className="App w-full min-h-screen bg-[#f5f2ed]">
       <Navbar />
       <Routes>
-        <Route path="/createcard" />
+        <Route path="/createcard" element={<CreateGroup />} />
         <Route path="/myflashcards" />
         <Route path="/:details" />
       </Routes>
