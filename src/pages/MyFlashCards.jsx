0ff5c4eb6b745f8ee8ef9c2cc9578d50394f2043
kolheggaurav <kolheg58@gmail.com>
2024-02-04 13:51:39 +0530

    Flashcard component completed

--- src/pages/MyFlashCards.jsx
+++ src/pages/MyFlashCards.jsx
@@ -1,7 +1,15 @@
+import FlashcardComponent from "../components/FlashcardComponent/FlashcardComponent";
+
 const MyFlashCards = () => {
   return (
-    <div className="px-5 md:px-10 lg:px-32 ">
+    <div className="grid grid-cols-1 px-5 md:px-10 lg:px-32 ">
       <>
+      <FlashcardComponent/>
+      <FlashcardComponent/>
+      <FlashcardComponent/>
+      <FlashcardComponent/>
+      <FlashcardComponent/>
+      <FlashcardComponent/>
       </>
       </div>
   );
