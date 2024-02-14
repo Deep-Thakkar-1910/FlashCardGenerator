935fe375f65c58672bddf705f15f78302baa765f
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-02-14 17:55:48 +0530

    Final Commit

--- src/components/ToastComponent/ToastComponent.jsx
+++ src/components/ToastComponent/ToastComponent.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/prop-types */
 import { BsCheckCircle } from "react-icons/bs";
 
-const Toast = ({ fn, toastClass }) => {
+const ToastComponent = ({ fn, toastClass }) => {
   return (
     <div
       data-testid="toast-dataid"
-      className={`${toastClass} fixed left-1/2 top-2 z-50 w-72 -translate-x-1/2 transform space-y-4 rounded-md bg-red-600 p-5 text-white transition-all duration-1000 ease-in-out sm:w-96`}
+      className={`${toastClass} fixed left-1/2 top-2 z-50 w-72 -translate-x-1/2 transform space-y-4 rounded-md bg-blue-600 p-5 text-white transition-all duration-1000 ease-in-out sm:w-96`}
     >
       <div>
         <h3 className="mb-2 flex flex-col items-center gap-2 text-xl font-semibold sm:flex-row">
-          <i className="text-2xl text-red-200">
+          <i className="text-2xl text-blue-200">
             <BsCheckCircle />
           </i>
           Your Flashcard is created.
         </h3>
-        <p className="text-red-200">
+        <p className="text-blue-200">
           Go to My Flashcard tab and check your all of your created flashcards.
         </p>
       </div>
@@ -22,7 +22,7 @@ const Toast = ({ fn, toastClass }) => {
         <button
           type="button"
           onClick={fn}
-          className="min-w-max rounded-md border-2 border-red-300 px-4 py-1 font-semibold text-red-200 transition-all hover:border-red-700 hover:bg-red-700 active:animate-ping"
+          className="min-w-max rounded-md border-2 border-blue-300 px-4 py-1 font-semibold text-blue-200 transition-all hover:border-blue-700 hover:bg-blue-700 active:animate-ping"
         >
           Dismiss
         </button>
@@ -31,4 +31,4 @@ const Toast = ({ fn, toastClass }) => {
   );
 };
 
-export default Toast;
+export default ToastComponent;
