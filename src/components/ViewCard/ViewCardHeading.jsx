136fa4280229592910b17458dd2899070e4fdf92
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-02-15 18:15:13 +0530

    Final Commit

--- src/components/ViewCard/ViewCardHeading.jsx
+++ src/components/ViewCard/ViewCardHeading.jsx
@@ -8,7 +8,9 @@ const ViewCardHeading = ({ viewCardHeading, viewCardDescription }) => {
         <IoMdArrowRoundBack className="cursor-pointer text-xl text-gray-700" />
       </Link>
       <div className="flex flex-col items-start">
-        <h1 className="text-2xl font-bold">{viewCardHeading}</h1>
+        <h1 className="text-2xl font-bold first-letter:uppercase">
+          {viewCardHeading}
+        </h1>
         <h4 className=" text-gray-500">{viewCardDescription}</h4>
       </div>
     </div>
